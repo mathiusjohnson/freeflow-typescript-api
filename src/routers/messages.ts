630480381357a8ad698be2a01951f86cb62f2ef0
dreamxinxcode <dreamxinxcode@gmail.com>
2020-11-14 19:08:56 -0800

    fixed endpoint

--- src/routers/messages.ts
+++ src/routers/messages.ts
@@ -2,12 +2,18 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 module.exports = (queryFunctions: any) => {
-	router.get('convo/:sender_id/:receiver_id', (req: Request, res: Response) => {
+	router.get('/', (req: Request, res: Response) => {
+
+		queryFunctions
+			.getAllMessages()
+			.then((resolve: Object) => res.send(resolve))
+			.catch((error: String) => console.log(error));
+  });
+
+  router.get('/convo/:sender_id/:receiver_id', (req: Request, res: Response) => {
 		const senderID = Number(req.params.sender_id);
     const receiverID = Number(req.params.receiver_id);
-    
-    console.log('Sender:', senderID, 'Receiver:', receiverID);
-    
+        
 		queryFunctions
 			.getConvo(senderID, receiverID)
 			.then((resolve: Object) => res.send(resolve))
