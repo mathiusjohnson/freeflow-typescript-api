014cad6864ddf8d407212c375fd6469aff5f9072
Angel <blacitea@gmail.com>
2020-11-18 20:14:12 -0800

    change params name to receiver_id

--- src/routers/karmas.ts
+++ src/routers/karmas.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 module.exports = (queryFunctions: QueryFunctions) => {
 	router.get('/user', (req: Request, res: Response) => {
-		const userId = req.body.giver_id; // ???
+		const userId = req.body.receiver_id;
 		queryFunctions
 			.getKarmaCountByUser(userId)
 			.then((resolve: object) => res.send(resolve))
